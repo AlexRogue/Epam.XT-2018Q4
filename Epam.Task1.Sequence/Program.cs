dc3438c4a188d6723d9a9698da32272f9c257e83
AlexRogue <men.0090@mail.ru>
2018-11-19 01:27:36 +0400

    Add project files.

---
+++ Epam.Task1.Sequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1.Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter your number. Press button Enter");
+            int number = new ResultParser().ParseResult();
+            List<int> collection = new List<int>();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 1; i <= number; i++)
+            {
+                collection.Add(i);
+            }
+            collection.ForEach(x =>
+            {
+                if (x != number)
+                {
+                    stringBuilder.Append(x).Append(", ");
+                }
+                else
+                {
+                    stringBuilder.Append(x).Append(" ");
+                }
+            });
+            Console.Write(stringBuilder);
+        }
+    }
+}
---
+++ Epam.Task1.Simple/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Epam.Task1.Simple
+{
+    class Program
+    {
+        public static void Main()
+        {
+            {
+                ResultParser resultParser = new ResultParser();
+                var number = resultParser.ParseResult();
+                CheckNumber ob = new CheckNumber(number);
+                if (ob.IsPrime())
+                {
+                    Console.WriteLine(number + " is a prime number.");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not a prime number.");
+                }
+             }
+        }
+    }
+
+    class CheckNumber
+    {
+        int number;
+
+        public CheckNumber(int number)
+        {
+            this.number = number;
+        }
+
+
+        public bool IsPrime()
+        {
+            for (int i = 2; i < number; i++)
+                if ((number % i) == 0) return false;
+            return true;
+        }
+    }
+}
