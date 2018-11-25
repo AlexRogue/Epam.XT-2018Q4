295fc3bc2c5674e0ea24f139fa6e59b503a6ac7e
AlexRogue <men.0090@mail.ru>
2018-11-25 05:35:31 +0400

    New tasks

---
+++ Epam.Task1.3.AnotherTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1._3.AnotherTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberoflayer = int.Parse(Console.ReadLine());
+            Console.WriteLine("Print paramid");
+            for (int i = 1; i <= numberoflayer; i++) 
+            {
+                for (int a = 1; a <= (numberoflayer - i); a++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = (i - 1); j >= 1; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ Epam.Task1.5.SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1._5.SumOfNumbers
+{
+    class Program
+    {
+        static List<int> list = new List<int>();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    list.Add(i);    
+                }
+            }
+           var y =  list.Sum();
+        }
+    }
+}
